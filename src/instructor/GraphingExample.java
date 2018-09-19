658e338d2d91b39991f98f588d4c9040cf9eef38
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-09-19 10:38:53 -0400

    class program sep 19

--- src/instructor/GraphingExample.java
+++ src/instructor/GraphingExample.java
@@ -14,7 +14,21 @@ public class GraphingExample extends Application {
     * various shapes.
     */
     public void start(Stage primaryStage) {
+      Line line = new Line (10, 50, 150, 200);
+      Circle circle = new Circle (100, 80, 20);
+      circle.setFill(Color.BLUE);
 
+      Rectangle rect = new Rectangle (50, 50, 50, 50);
+      rect.setStroke(Color.RED);
+      rect.setStrokeWidth(2);
+      rect.setFill(null);
+
+      Group root = new Group(line, circle, rect);
+      Scene scene = new Scene(root, 400, 200);
+
+      primaryStage.setTitle("Example");
+      primaryStage.setScene(scene);
+      primaryStage.show();
     }
 
     public static void main(String[] args)
