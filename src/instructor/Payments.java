8fc263d1927cd36409bee5a6c648b437234a9acc
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-10-03 09:56:15 -0400

    Payments.java modified

--- src/instructor/Payments.java
+++ src/instructor/Payments.java
@@ -7,7 +7,6 @@ public class Payments {
 		double loanAmount;
 		double rate;
 		int loanTerm;
-
 		// create an instance of the Scanner
 		Scanner scan = new Scanner (System.in);
 
@@ -19,10 +18,17 @@ public class Payments {
 		System.out.println("Enter loan term");
 		loanTerm = scan.nextInt();
 
+		// apply amortization formula
+		// calculate the term for the power function
 		double term = Math.pow(1 + rate, loanTerm);
 		System.out.println("Term is: " + term);
 
 		double payment = loanAmount * rate * (term / (term - 1));
 		System.out.println("Payment: "+ payment);
+
+		// unrelated: demonstration of the conversion from String to int
+		String myString = "546";
+		int myInteger = Integer.parseInt(myString);
+		System.out.println("My integer " + myInteger);
 	}
 }
