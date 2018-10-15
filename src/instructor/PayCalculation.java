2491b3bc2d943d642f8ff744e660bad6cd242afa
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-10-15 10:19:48 -0400

    Updated PayCalculation program.

--- src/instructor/PayCalculation.java
+++ src/instructor/PayCalculation.java
@@ -1,26 +1,49 @@
-
 import java.util.Scanner;
 
+/** Program to calculate employee pay.
+*/
 public class PayCalculation {
-  public static void main ( String [] args ) {
-    Scanner input = new Scanner ( System.in );
-    System.out.print ( "Base salary: " );
-    int salary = input.nextInt();
-    System.out.print ( "Pay grade: " );
+  /** Use nested if statements
+  to check paygrade and level conditions.
+  */
+  public static void main (String [] args) {
+    Scanner input = new Scanner (System.in);
+
+    // get the user's input
+    System.out.println(" Enter base salary");
+    double salary = input.nextDouble();
+    System.out.println(" Enter paygrade");
     int paygrade = input.nextInt();
-    System.out.print ( "Level: " );
+    System.out.println(" Enter level");
     int level = input.nextInt();
 
-    if (paygrade == 1) {
-      if (level >= 2 && level <= 4) {
-        salary *= 1.05;
+    double newSalary = salary;
+
+    if(paygrade == 1) {
+      if(level >= 3 && level <= 4) {
+        newSalary *= 1.05;
+      }
+      else if (level == 1 || level == 2){
+        newSalary *= 1.02;
       }
       else {
-        salary *= 1.04;
+        System.out.println("Invalid level input");
       }
+    } else if (paygrade == 2) {
+      if(level >= 3 && level <=4) {
+        newSalary *= 1.08;
+      }
+      else if (level == 1 || level == 2){
+        newSalary *= 1.04;
+      }
+      else {
+        System.out.println("Invalid level input");
+      }
+    } else {
+      System.out.println("Incorrect paygrade");
     }
-    else {
-      salary *= 1.06;
-    }
+
+
+    System.out.println("Salary is " + newSalary);
   }
 }
