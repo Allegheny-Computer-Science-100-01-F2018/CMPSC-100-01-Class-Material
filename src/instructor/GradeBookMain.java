278353c5f276d81a5555c16adddfe3dc6c2ba4b0
Janyl Jumadinova <jjumadinova@allegheny.edu>
2018-10-16 16:10:13 -0400

    Class programs for October 17

---
+++ src/instructor/GradeBookMain.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/** Create a class GradeBookMain to instantiate GradeBook class
+*/
+
+public class GradeBookMain {
+  /** main method where execution begins.
+  */
+  public static void main (String args[]) {
+
+    GradeBook myGradeBook  = new GradeBook ();
+    myGradeBook.displayMessage();
+  }
+}
